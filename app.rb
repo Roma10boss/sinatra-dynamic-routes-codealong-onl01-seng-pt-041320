aed6372fba5a1577e3a59fb95b91f9b9f41f08e5
Romario Gustave <romagustave10@gmail.com>
2020-07-04 19:22:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,14 +15,5 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
   
-    get '/goodbye' do
-    "Goodbye!"
-  end
-
-  # This is a sample dynamic route.
-  get "/goodbye/:name" do
-    @user_name = params[:name]
-    "Goodbye #{@user_name}!"
-  end
-
+ 
 end
