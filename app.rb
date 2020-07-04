4cb56d50437e7b61a50e54856edf03c7ed2088a8
Romario Gustave <romagustave10@gmail.com>
2020-07-04 19:23:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,5 +15,23 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
   
+    get '/reversename/:name' do
+    @reverse_name = params[:name].reverse
+    @reverse_name
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i ** 2
+    @num.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+  end
+
  
 end
